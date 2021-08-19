8cf71ba9c69b5992e9a54b4aa505744affd1d3bd
Dyleme <aleksei.dyavgo@gmail.com>
2021-08-19 18:02:24 +0300

    fixing work with UTF-8

--- task1.go
+++ task1.go
@@ -2,13 +2,18 @@ package task1
 
 import (
 	"strings"
+	"unicode/utf8"
 )
 
 func ReverseString(str string) string {
 	var sb strings.Builder
-	sb.Grow(len(str))
-	for i := len(str) - 1; i >= 0; i-- {
-		sb.WriteByte(str[i])
+	b := []byte(str)
+	length := len(str)
+	composeWidth := 0
+	for composeWidth < length {
+		r, width := utf8.DecodeLastRune(b[:length-composeWidth])
+		sb.WriteRune(r)
+		composeWidth += width
 	}
 	return sb.String()
 }
--- task1_test.go
+++ task1_test.go
@@ -7,6 +7,8 @@ import (
 func TestReverseString(t *testing.T) {
 	t.Run("Hello world", testReverseStringFunc("Hello world!", "!dlrow olleH"))
 	t.Run("123MaiN678", testReverseStringFunc("123MaiN678", "876NiaM321"))
+	t.Run("Привет", testReverseStringFunc("Привет", "тевирП"))
+	t.Run("日本語", testReverseStringFunc("日本語", "語本日"))
 }
 
 func testReverseStringFunc(str string, expexted string) func(t *testing.T) {
